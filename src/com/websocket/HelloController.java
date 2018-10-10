4175f044a8dd04fd19aa7b41e7fb27166c189a03
wandererparsifal <wandererparsifal@gmail.com>
2018-10-10 14:30:08 +0800

    配置工程

---
+++ src/com/websocket/HelloController.java
@@ -0,0 +1,18 @@
+package com.websocket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+//@RequestMapping(value = "/hello", method = RequestMethod.GET)
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("title", "Hello");
+        model.addAttribute("content", "Spring MVC Hello World");
+        return "hello";
+    }
+}
