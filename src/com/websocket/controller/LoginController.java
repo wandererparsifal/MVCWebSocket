a849f2f35f5d766f3b5a91e39a98f774a9ed4f99
wandererparsifal <wandererparsifal@gmail.com>
2018-10-12 17:01:27 +0800

    验证用户名密码

--- src/com/websocket/controller/LoginController.java
+++ src/com/websocket/controller/LoginController.java
@@ -1,13 +1,15 @@
 package com.websocket.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.websocket.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.ArrayList;
 
 @Controller
 public class LoginController {
@@ -21,28 +23,74 @@ public class LoginController {
     public void login(HttpServletRequest request, HttpServletResponse response) {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        System.out.println("username " + username + ", password " + password);
+        String path = request.getSession().getServletContext().getRealPath("/storage/user.json");
+        String usersJson = readFile(path);
+        ArrayList<User> users = null;
+        if (!StringUtils.isEmpty(usersJson)) {
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                users = mapper.readValue(usersJson,
+                        mapper.getTypeFactory().constructParametricType(ArrayList.class, User.class));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json");
         PrintWriter writer;
-        ResponseBean responseBean;
+        ResponseBean responseBean = null;
+        if (null != users) {
+            boolean found = false;
+            for (User user : users) {
+                if (user.getName().equals(username)) {
+                    if (user.getPwd().equals(password)) {
+                        responseBean = new ResponseBean(true, false, "");
+                    } else {
+                        responseBean = new ResponseBean(false, true, "Wrong password");
+                    }
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                responseBean = new ResponseBean(false, true, "No such user");
+            }
+        } else {
+            responseBean = new ResponseBean(false, true, "No such user");
+        }
         try {
             writer = response.getWriter();
             ObjectMapper mapper = new ObjectMapper();
-            if ("aa".equals(username) && "vv".equals(password)) {
-                responseBean = new ResponseBean(true, false, "");
-                String json = mapper.writeValueAsString(responseBean);
-                writer.write(json);
-            } else {
-                responseBean = new ResponseBean(false, true, "wrong password");
-                String json = mapper.writeValueAsString(responseBean);
-                writer.write(json);
-            }
+            String json = mapper.writeValueAsString(responseBean);
+            writer.write(json);
             writer.flush();
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String readFile(String path) {
+        String result = null;
+        File file = new File(path);
+        if (file.exists()) {
+            InputStream inputStream;
+            try {
+                inputStream = new FileInputStream(file);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(buffer)) != -1) {
+                    byteArrayOutputStream.write(buffer, 0, length);
+                }
+                result = byteArrayOutputStream.toString("UTF-8");
+                inputStream.close();
+                byteArrayOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 
     class ResponseBean {
